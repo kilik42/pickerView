8841a13d9ea6d430af917950192f3c8c0b760b6d
marvin evins <maevins@cps.edu>
2016-11-19 20:13:38 -0600

    commit of picker view

--- pickerView/ViewController.swift
+++ pickerView/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    var bikes : [String] = ["street boby", "Street 769", "fat bob", "iron 883"]
+    
+    @IBOutlet var pickerView: UIPickerView!
+    
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        pickerView.dataSource = self
+        pickerView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return bikes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        let myBike = bikes[row]
+        print(myBike)
+        label.text = "\(myBike)"
+        return myBike
+        
+    }
+    
+   
 
 }
 
